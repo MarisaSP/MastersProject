3bcb8f1f55087a78408698f38630f498e16d81dc
marisapatel <marisapatel@100.67.211.72>
2021-10-20 15:55:01 +0100

    Added to GUI

--- MastersProject/src/MainGUI.java
+++ MastersProject/src/MainGUI.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 public class MainGUI {
 
@@ -34,8 +40,33 @@ public class MainGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setForeground(Color.WHITE);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("FC[REG]-CQ");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 13));
+		lblNewLabel.setHorizontalAlignment(JLabel.CENTER);
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.WEST);
+		
+		JLabel lblNewLabel_1 = new JLabel("Create Table");
+		panel.add(lblNewLabel_1);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.EAST);
+		
+		JLabel lblNewLabel_2 = new JLabel("Query Tables");
+		panel_1.add(lblNewLabel_2);
+		
+		JPanel panel_2 = new JPanel();
+		frame.getContentPane().add(panel_2, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_3 = new JLabel("Display Table");
+		panel_2.add(lblNewLabel_3);
 	}
 
 }
